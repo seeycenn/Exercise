35b98b84835b35c406528fcd2e0145eb2dd6a274
YeeShuen <91144543+seeycenn@users.noreply.github.com>
2021-11-24 13:29:27 +0800

    comments added

--- PalindromeCheck.cpp
+++ PalindromeCheck.cpp
@@ -6,21 +6,21 @@ int main(){
 	int n,re;
 	cin>>n;
 	if(n<=0){
-		cout<<"+ve num"<<endl;
+		cout<<"pls enter a +ve num"<<endl;
 		cin>>n;
 	}
-	int n2=n,n3;
+	int n2=n,n3;	//n2 use for condition check in while loop, n3 use in calc
 	while(n2>0){
-        n3=n2%10;
-        if(n2>10)
-		    re=(re+n3)*10;
-        else
-            re=re+n3;
-        n2/=10;
+        	n3=n2%10;
+        	if(n2>10)
+			re=(re+n3)*10;
+        	else
+        		re=re+n3;
+        	n2/=10;
 	}
 	cout<<re<<endl;
-    if(re==n)
-        cout<<"is p"<<endl;
+    	if(re==n)
+        	cout<<"is p"<<endl;
 		
-  return 0;
+  	return 0;
   }
